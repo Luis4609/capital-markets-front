85e595b4d8ff3ad3f237e60f8232e3bf9804316c
Luis4609 <luis.monzon.pedreira@alumnos.upm.es>
2022-08-02 15:02:47 +0200

    Config for vitetest added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Footer from "../components/Footer";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -13,23 +13,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-         Capital Markets
-        </h1>
+        <h1 className={styles.title}>Capital Markets</h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer></Footer>
     </div>
   );
 };
