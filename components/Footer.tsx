a53821b91b0a27bc307036557c8b50b717221ebf
Luis Monzon <luis.monzon.pedreira@alumnos.upm.es>
2022-08-10 15:14:16 +0200

    Update Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,47 @@
-import styles from "../styles/Footer.module.css";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
 
-function Footer() {
+function Copyright() {
   return (
-    <footer className={styles.footer}>
-      <p>© 2022 Capital Markets All rights reserved</p>
-    </footer>
+    <Typography variant="body2" color="text.secondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Capital Markets
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
   );
 }
+
+const Footer = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: "auto",
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            My sticky footer can be found here.
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+    </>
+  );
+};
 
 export default Footer;
