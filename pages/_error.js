462d7418881364bddd1308239e3fa15a88bad0f0
Luis Monzon <luis.monzon.pedreira@alumnos.upm.es>
2022-08-22 14:56:32 +0200

    Add styles to custom error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,10 +1,21 @@
+import { Typography } from "@mui/material";
+import { Container } from "@mui/system";
+
+import { red } from "@mui/material/colors";
+
 const Error = ({ statusCode }) => {
   return (
-    <p>
-      {statusCode
-        ? `An error ${statusCode} ocurred on the server`
-        : "An error occurred on the client"}
-    </p>
+    <Container>
+      {statusCode ? (
+        <Typography variant="h5" sx={{ color: red[500] }}>
+          An error {statusCode} ocurred on the server
+        </Typography>
+      ) : (
+        <Typography variant="h5" sx={{ color: red[500] }}>
+          An error occurred on the client
+        </Typography>
+      )}
+    </Container>
   );
 };
 
