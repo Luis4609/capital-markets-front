e910fce0487ac17d0256c3655113ee619e2e311d
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2022-08-27 12:33:52 +0200

    Delete cypress files

--- cypress/component/Footer.cy.tsx
+++
@@ -1,8 +0,0 @@
-import Footer from "../../components/Footer";
-
-describe("<Footer.cy.tsx>", () => {
-  it("mounts", () => {
-    const onChangeSpy = cy.spy().as("onChangeSpy");
-    cy.mount(<Footer />);
-  });
-});
